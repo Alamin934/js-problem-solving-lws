cf869cf08d414dcd29351776189a19986f625210
Alamin934 <islamalamin934@gmail.com>
2022-06-07 11:26:57 +0600

    solve lodu game prolbem

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//
+//How can we print 1-6 number randomly in LODU game?
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRndInteger(1, 6);
