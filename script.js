4f9421ad45d8eb615900fc6453d69bebcd741bee
Alamin934 <islamalamin934@gmail.com>
2022-06-07 11:41:42 +0600

    solve numbers sorting using array numeric sort method

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ getRndInteger(1, 6);
 const students = ["Abir", "Shams", "Kiron", "Rahim", "Hasan", "Sajjad"];
 
 console.log(students.sort());
+
+
+
+// How do we sort the Roll numbers of everyone in our classroom seriully?
+
+const roll_numbers = [3, 2, 6, 4, 75, 34, 23, 5, 8, 7];
+
+console.log(roll_numbers.sort((a, b) => { return a - b }))
